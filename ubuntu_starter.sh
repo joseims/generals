373978c71942e317dcca191e67a33e791cb4ec50
Ignácio Schmid <jose.schmid@ccc.ufcg.edu.br>
2019-06-04 07:11:04 -0300

    Add files via upload

---
+++ ubuntu_starter.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - #chrome
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+
+sudo apt updatesudo apt install software-properties-common apt-transport-https wget #vscode
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+sudo apt install python-pip #pip
+
+
+#slack
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb /tmp
+sudo apt install /tmp/slack-desktop-*.deb
+
+#discord
+
+#git
+
+
+
+#steam
+sudo apt update && sudo apt install steam
+
+
+#spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+
+
+
+#tree
+
